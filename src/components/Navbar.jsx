7fd6aaf9eb6c2875e59552eb1540d53a395d798a
dtran <dtran13899@gmail.com>
2022-09-22 21:46:51 -0500

    Structure the project and add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  function handleToggle() {
+    setToggle(prevToggle => !prevToggle);
+  }
+
+  return (
+    <nav className="w-full flex justify-between items-center py-6 navbar">
+      <img src={logo} alt="HooBank" className="w-[124px] h-[32px]" />
+
+      <ul className="sm:flex hidden justify-end items-center flex-1 list-none">
+        {navLinks.map((link, index) => (
+          <li
+            key={link.id}
+            className={`font-poppins font-normal cursor-pointer text-base text-white 
+            ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
+          >
+            <a href={`#${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex justify-end items-center flex-1">
+        <img
+          src={toggle ? close : menu}
+          alt="Menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={handleToggle}
+        />
+
+        <div
+          className={`${
+            toggle ? 'flex' : 'hidden'
+          } bg-black-gradient absolute top-20 right-0 p-6 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="flex flex-col justify-end items-center flex-1 list-none">
+            {navLinks.map((link, index) => (
+              <li
+                key={link.id}
+                className={`font-poppins font-normal cursor-pointer text-base text-white 
+                ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'}`}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
