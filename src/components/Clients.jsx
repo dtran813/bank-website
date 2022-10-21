ca6afccc305b463e3076dc2f393fee7f5d1a84d8
dtran <dtran13899@gmail.com>
2022-10-20 19:51:15 -0500

    Add Clients and CTA components

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { clients } from '../constants';
+import styles from '../style';
 
-const Clients = () => {
-  return <div>Clients</div>;
-};
+const Clients = () => (
+  <section id="clients" className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map(client => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} md:min-w-min min-w-max m-5`}
+        >
+          <img
+            src={client.logo}
+            alt={`Logo of ${client.logo}`}
+            className="sm:w-48 w-24 object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
