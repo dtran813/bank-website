d174237dab14337a66847bfe4f4a84c0c017fe4f
dtran <dtran13899@gmail.com>
2022-10-20 21:53:21 -0500

    Minor fix for Stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,9 +3,7 @@ import { stats } from '../constants';
 import styles from '../style';
 
 const Stats = () => (
-  <section
-    className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 sm:mt-20 mb-6 px-14`}
-  >
+  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
     {stats.map(stat => (
       <div
         key={stat.id}
